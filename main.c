565fb15091b2314617fdbac4a801a381fbb5299f
Alon A. Rabinowitz <alonrab@outlook.com>
2024-02-17 16:37:42 +0200

    Fin

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <xc.h>
 #include "System/system.h"
 #include "System/delay.h"
@@ -23,7 +22,7 @@ typedef struct
     u16 red;
     u16 green;
     u16 blue;
-    u16 current_potentiometer_value;
+    u16 prev_potentiometer_value;
     color_t current_color;
 } RGB_t;
 
@@ -33,6 +32,8 @@ void in_out_initialize(void)
     TRISAbits.TRISA11 = 1; // button S1
     TRISAbits.TRISA12 = 1; // button s2
     TRISBbits.TRISB12 = 1; // potentiomitor
+    TRISAbits.TRISA8 = 0;  // LED1
+    TRISAbits.TRISA9 = 0;  // LED2
     RPOR13bits.RP26R = 13; // connecting OC1 to RP26 aka RPOR13[0:7]
     RPOR13bits.RP27R = 14;
     RPOR11bits.RP23R = 15;
@@ -80,11 +81,11 @@ void RGB_set_span()
 
 void main_initialize(void)
 {
+    SYSTEM_Initialize();
     in_out_initialize();
     a_to_d_convertor_initialize();
     RGB_initialize();
     RGB_set_span();
-    SYSTEM_Initialize();
 }
 
 u16 potentiometer_value()
@@ -101,29 +102,42 @@ u16 potentiometer_value()
     return ADC1BUF0;
 }
 
+// mode = 0
 // changes to the next color in the sequence e.g. red -> green -> blue -> red etc...
 void set_RGB_color(RGB_t *rgb)
 {
-    u16 potentiometer_value = rgb->current_potentiometer_value;
     switch (rgb->current_color)
     {
     case RED:
-        OC1R = potentiometer_value;
-        rgb->red = potentiometer_value;
+        OC1R = rgb->red;
         OC2R = 0;
         OC3R = 0;
         break;
     case GREEN:
-        OC2R = potentiometer_value;
-        rgb->green = potentiometer_value;
+        OC2R = rgb->green;
         OC1R = 0;
         OC3R = 0;
         break;
     case BLUE:
-        OC3R = potentiometer_value;
-        rgb->blue = potentiometer_value;
+        OC3R = rgb->blue;
         OC1R = 0;
         OC2R = 0;
+        break;
+    }
+}
+
+void update_color(RGB_t *rgb)
+{
+    switch (rgb->current_color)
+    {
+    case RED:
+        rgb->red = rgb->prev_potentiometer_value;
+        break;
+    case GREEN:
+        rgb->green = rgb->prev_potentiometer_value;
+        break;
+    case BLUE:
+        rgb->blue = rgb->prev_potentiometer_value;
         break;
     }
 }
@@ -131,7 +145,7 @@ void set_RGB_color(RGB_t *rgb)
 // mode = 1
 void composite_mode(RGB_t *rgb)
 {
-    double relative_pot = rgb->current_potentiometer_value / 1023;
+    float relative_pot = (rgb->prev_potentiometer_value / 1023.0);
     OC1R = rgb->red * relative_pot;
     OC2R = rgb->green * relative_pot;
     OC3R = rgb->blue * relative_pot;
@@ -142,10 +156,10 @@ void swap_mode(u4 mode_select, RGB_t *rgb)
     switch (mode_select)
     {
     case 0:
-        set_RGB_color(&rgb);
+        set_RGB_color(rgb);
         break;
     case 1:
-        composite_mode(&rgb);
+        composite_mode(rgb);
         break;
     }
 }
@@ -159,42 +173,59 @@ int main(void)
     u4 s1_pressed = 0;
     u4 s2_pressed = 0;
     u16 pot = 0;
-    // u4 flag = 0;
-    color_t color = RED;
-    RGB_t rgb = {0, 0, 0, 0, RED}; // red, green, blue, potentiometer value, current color
-    main_initialize();           // initialize the system
+    RGB_t rgb = {512, 512, 512, 0, RED}; // red, green, blue, potentiometer value, current color
+    main_initialize();                   // initialize the system
 
     // main loop
     while (1)
     {
         pot = potentiometer_value();
-        if (pot + 25 < rgb.current_potentiometer_value || pot - 25 > rgb.current_potentiometer_value)
+        // if the potentiometer value changes by 25
+        if ((pot + 25) % 1024 < rgb.prev_potentiometer_value || (pot - 25) % 1024 > rgb.prev_potentiometer_value)
         {
-            rgb.current_potentiometer_value = pot;
-            // flag = 1;
+            rgb.prev_potentiometer_value = pot;
+            // if mode_select is 0 (individual color), then update that color
+            mode_select ? NULL : update_color(&rgb);
         }
         // if button S1 is pressed
         // check if the button s1 is held down
         // if it is held down, then only do this once
         // e.g. the value of pressed will be 1 after the first iteration.
-        if (PORTAbits.RA11 == 0 && s1_pressed == 0)
+        if (PORTAbits.RA11 == 0)
         {
-            s1_pressed = 1;
-            mode_select = (mode_select + 1) % 2;
-            swap_mode(mode_select, &rgb);
+            LATAbits.LATA8 = 1;
+            if (s1_pressed == 0)
+            {
+                s1_pressed = 1;
+                mode_select = (mode_select == 0) ? 1 : 0;
+            }
         }
         else
+        {
             s1_pressed = 0;
-
-        // button S2 is pressed and mode_select is individual
-        if (PORTAbits.RA12 == 0 && mode_select == 0 && s2_pressed == 0)
-        {
-            s2_pressed = 1;
-            set_RGB_color(&rgb);
-            // PORTCbits.RC7 = 0; ?? what is this for?
+            LATAbits.LATA8 = 0;
         }
-        else
-            s2_pressed = 0;
+
+        if (mode_select == 0)
+        {
+            if (PORTAbits.RA12 == 0)
+            {
+                LATAbits.LATA9 = 1;
+                if (s2_pressed == 0)
+                {
+                    s2_pressed = 1;
+                    rgb.current_color = (rgb.current_color + 1) % 3;
+                }
+            }
+            else
+            {
+                LATAbits.LATA9 = 0;
+                s2_pressed = 0;
+            }
+
+            set_RGB_color(&rgb);
+        }
+        // button S2 is pressed and mode_select is individual
 
         if (mode_select == 1)
         {
